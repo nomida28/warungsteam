76e4a790179309d6c78c3c7cb871488d34801503
nomida28 <nadazria9@gmail.com>
2024-10-09 14:51:45 +0700

    Update server.js

--- live-chat-music/api/server.js
+++ live-chat-music/api/server.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
-app.use(express.static('public')); // File statis disajikan dari folder 'public'
+app.use(cors());
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log('User connected');
 
-    // Saat pesan diterima dari klien
     socket.on('message', (data) => {
-        io.emit('message', data); // Kirim kembali ke semua pengguna
+        io.emit('message', data);
     });
 
-    // Saat ada permintaan lagu
     socket.on('songRequest', (data) => {
-        io.emit('songRequest', data); // Kirim permintaan lagu ke semua pengguna
+        io.emit('songRequest', data);
     });
 
     socket.on('disconnect', () => {
